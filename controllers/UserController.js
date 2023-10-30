1372bae091c810eca61d0c4e4bfdf98be0b1824b
pramesthias <oka.pramesthi@gmail.com>
2023-10-30 22:17:26 +0700

    Feat: feature edit User by id

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -51,23 +51,23 @@ module.exports = class UserController {
     }
 
     //PUT
-    // static async editUser(req, res){
-    //     try {
-    //         let user = await User.findByPk(req.params.id);
-    //         if(!user) throw ({name: "NotFound"});
-    //         await user.update(req.body);
-    //         res.status(200).json(req.body);
-    //     } catch (error) {
-    //         console.log(error.name);
-    //         if(error.name === "SequelizeValidationError" || error.name === "SequelizeUniqueConstraintError"){
-    //             res.status(400).json({message: error.errors[0].message}); 
-    //         } else if(error.name === "NotFound") {
-    //             res.status(404).json({message: "error not found"});
-    //         } else {
-    //             res.status(500).json({message: "Internal Server Error"});
-    //         }
-    //     }
-    // }
+    static async editUser(req, res){
+        try {
+            let user = await User.findByPk(req.params.id);
+            if(!user) throw ({name: "NotFound"});
+            await user.update(req.body);
+            res.status(200).json(req.body);
+        } catch (error) {
+            console.log(error.name);
+            if(error.name === "SequelizeValidationError" || error.name === "SequelizeUniqueConstraintError"){
+                res.status(400).json({message: error.errors[0].message}); 
+            } else if(error.name === "NotFound") {
+                res.status(404).json({message: "error not found"});
+            } else {
+                res.status(500).json({message: "Internal Server Error"});
+            }
+        }
+    }
 
     //DELETE
     // static async deleteUser(req, res){
