016e37d4b402158ca4fbe9f1981d551707950efc
pramesthias <oka.pramesthi@gmail.com>
2023-10-30 23:01:24 +0700

    Feat: feature read all categories

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -4,22 +4,16 @@ module.exports = class CategoryController {
 
     static async getCategories(req, res){
         try {
-            const category = await Category.findAll(); 
-            res.status(200).json(category);
+            const categories = await Category.findAll(); 
+            res.status(200).json(categories);
         } catch (error) {
             console.log(error);
             res.status(500).json({message: "Internal Server Error"})
         }
     }
 
-    static async getUser(req, res){
-        try {
-            const user = await User.findByPk(req.params.id);
-            res.status(200).json(user);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({message: "Internal Server Error"})
-        }
-    }
+
+
+
 
 }
