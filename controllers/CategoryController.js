1f10219dec0949624081185b9eac6c68895b0616
pramesthias <oka.pramesthi@gmail.com>
2023-10-30 23:01:56 +0700

    Feat: feature delete category

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -13,7 +13,20 @@ module.exports = class CategoryController {
     }
 
 
-
+    static async createCategory(req, res){
+        try {
+            console.log(req.body)
+            const category = await Category.create(req.body);
+            res.status(201).json(category);
+        } catch (error) {
+            console.log(error.name);
+            if(error.name === "SequelizeValidationError" || error.name === "SequelizeUniqueConstraintError"){
+                res.status(400).json({message: error.errors[0].message});
+                return; 
+            }
+            res.status(500).json({message: "Internal Server Error"});
+        }
+    }
 
 
 }
